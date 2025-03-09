f1ce5616ef85d0d4f8de4a898875dec22a41b06d
Tyler Cadena <tylercadena@alum.calarts.edu>
2025-03-08 21:09:59 -0800

    swap_chain organize

--- src/video/swap-chain.cpp
+++ src/video/swap-chain.cpp
@@ -10,75 +10,80 @@ namespace swap_chain {
 	glm::ivec2 dimensions_ {};
 	color_type color_ { color_type::BASE() };
 	blending_type blending_ { blending_type::alpha };
-	// public
-	void reset() {
-		glCheck(glEnable(GL_BLEND));
-		glCheck(glBlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD));
-		glCheck(glBlendFuncSeparate(
-			GL_SRC_ALPHA,
-			GL_ONE_MINUS_SRC_ALPHA,
-			GL_ONE,
-			GL_ONE_MINUS_SRC_ALPHA
-		));
+}
 
-		dimensions_ = {};
-		color_ = color_type::BASE();
-		blending_ = blending_type::alpha;
+// public
+void swap_chain::reset() {
+	glCheck(glEnable(GL_BLEND));
+	glCheck(glBlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD));
+	glCheck(glBlendFuncSeparate(
+		GL_SRC_ALPHA,
+		GL_ONE_MINUS_SRC_ALPHA,
+		GL_ONE,
+		GL_ONE_MINUS_SRC_ALPHA
+	));
+
+	dimensions_ = {};
+	color_ = color_type::BASE();
+	blending_ = blending_type::alpha;
+}
+
+void swap_chain::clear(const color_type& color) {
+	if (color_ != color) {
+		color_ = color;
+		glCheck(glClearColor(
+			cast<r32>(color_.r) / 255.0f,
+			cast<r32>(color_.g) / 255.0f,
+			cast<r32>(color_.b) / 255.0f,
+			cast<r32>(color_.a) / 255.0f
+		));
 	}
-	void clear(const color_type& color) {
-		if (color_ != color) {
-			color_ = color;
-			glCheck(glClearColor(
-				cast<r32>(color_.r) / 255.0f,
-				cast<r32>(color_.g) / 255.0f,
-				cast<r32>(color_.b) / 255.0f,
-				cast<r32>(color_.a) / 255.0f
+	glCheck(glClear(GL_COLOR_BUFFER_BIT));
+}
+
+void swap_chain::viewport(const glm::ivec2& dimensions) {
+	if (dimensions_ != dimensions) {
+		dimensions_ = dimensions;
+		glCheck(glViewport(0, 0, dimensions_.x, dimensions_.y));
+	}
+}
+
+const glm::ivec2& swap_chain::viewport() {
+	return dimensions_;
+}
+
+void swap_chain::blend(blending_type blending) {
+	if (blending_ != blending) {
+		switch (blending_ = blending; blending_) {
+		case blending_type::alpha: {
+			glCheck(glBlendFuncSeparate(
+				GL_SRC_ALPHA,
+				GL_ONE_MINUS_SRC_ALPHA,
+				GL_ONE,
+				GL_ONE_MINUS_SRC_ALPHA
 			));
+			break;
 		}
-		glCheck(glClear(GL_COLOR_BUFFER_BIT));
-	}
-	void viewport(const glm::ivec2& dimensions) {
-		if (dimensions_ != dimensions) {
-			dimensions_ = dimensions;
-			glCheck(glViewport(0, 0, dimensions_.x, dimensions_.y));
+		case blending_type::add: {
+			glCheck(glBlendFuncSeparate(
+				GL_SRC_ALPHA,
+				GL_ONE,
+				GL_ONE,
+				GL_ONE
+			));
+			break;
 		}
-	}
-	const glm::ivec2& viewport() {
-		return dimensions_;
-	}
-	void blend(blending_type blending) {
-		if (blending_ != blending) {
-			switch (blending_ = blending; blending_) {
-			case blending_type::alpha: {
-				glCheck(glBlendFuncSeparate(
-					GL_SRC_ALPHA,
-					GL_ONE_MINUS_SRC_ALPHA,
-					GL_ONE,
-					GL_ONE_MINUS_SRC_ALPHA
-				));
-				break;
-			}
-			case blending_type::add: {
-				glCheck(glBlendFuncSeparate(
-					GL_SRC_ALPHA,
-					GL_ONE,
-					GL_ONE,
-					GL_ONE
-				));
-				break;
-			}
-			case blending_type::multiply: {
-				glCheck(glBlendFuncSeparate(
-					GL_DST_COLOR,
-					GL_ZERO,
-					GL_DST_COLOR,
-					GL_ZERO
-				));
-				break;
-			}
-			default:
-				break;
-			}
+		case blending_type::multiply: {
+			glCheck(glBlendFuncSeparate(
+				GL_DST_COLOR,
+				GL_ZERO,
+				GL_DST_COLOR,
+				GL_ZERO
+			));
+			break;
+		}
+		default:
+			break;
 		}
 	}
 }
