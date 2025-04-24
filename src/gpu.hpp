067fe102fdb77d06cf9eaf496e577137e2a2b6b7
Tyler Cadena <tylercadena@alum.calarts.edu>
2025-04-24 01:44:13 -0700

    gpu.hpp commented out for now

--- src/gpu.hpp
+++ src/gpu.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-extern "C" __declspec(dllexport) unsigned long NvOptimusEnablement = 1;
-extern "C" __declspec(dllexport) unsigned long AmdPowerXpressRequestHighPerformance = 1;
+// extern "C" __declspec(dllexport) unsigned long NvOptimusEnablement = 1;
+// extern "C" __declspec(dllexport) unsigned long AmdPowerXpressRequestHighPerformance = 1;
